5c18508e496216bca6ffa069baaa5b3607edc574
Edrian Mariñas <edrian.a.marinas@gmail.com>
2026-06-21 15:43:22 +0800

    chore(deps): cleanup dependencies and reorganize user registration forms

--- src/pages/management/ManageUsersPage.tsx
+++ src/pages/management/ManageUsersPage.tsx
@@ -1,7 +1,7 @@
 import { useAuthStore } from '@/store/auth'
 import { ROLES } from '@/constants'
-import OwnerRegisterForm from '@/pages/management/__components/OwnerRegisterForm'
-import PmRegisterForm from '@/pages/management/__components/PmRegistrerForm'
+import OwnerRegisterForm from '@/pages/management/__components/owner/RegisterForm'
+import PmRegisterForm from '@/pages/management/__components/pm/RegisterForm'
 
 export default function RegisterPage() {
   const { user } = useAuthStore()
