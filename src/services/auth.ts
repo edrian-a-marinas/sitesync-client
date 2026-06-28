1994f5ae48a01548944783c57076cb62e2d89422
Edrian Mariñas <edrian.a.marinas@gmail.com>
2026-06-28 19:36:41 +0800

    fix: outdated import module fix

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,7 @@
 import { loginRequest, getMeRequest, registerRequest } from "@/api/auth";
 import { saveToken, clearToken } from "@/lib/token";
-import type { LoginInput, RegisterInput, TokenResponse, UserResponse } from "@/validations/auth";
+import type { LoginInput, TokenResponse, UserResponse } from "@/validations/auth";
+import type { RegisterInput } from "@/validations/user";
 
 export const loginUser = async (data: LoginInput): Promise<UserResponse> => {
   const tokenRes: TokenResponse = (await loginRequest(data)).data;
