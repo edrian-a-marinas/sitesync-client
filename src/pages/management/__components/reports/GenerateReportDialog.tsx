798c5d5b1b15afcded4b26d05625cadd5bbd1082
Edrian Mariñas <edrian.a.marinas@gmail.com>
2026-06-30 19:24:09 +0800

    chore(lint): suppress intentional React Hooks state reset warnings

--- src/pages/management/__components/reports/GenerateReportDialog.tsx
+++ src/pages/management/__components/reports/GenerateReportDialog.tsx
@@ -35,6 +35,7 @@ export default function GenerateReportDialog({
   const [cooldownLeft, setCooldownLeft] = useState(0)
   useEffect(() => {
     if (!cooldownUntil) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect -- resets cooldown when timer target is cleared
       setCooldownLeft(0)
       return
     }
